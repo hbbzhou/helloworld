6a1fc9fddf81a9db153e0690cccb5c41ddfc26ec
huangbinbinzhou <huang_binbin1@163.com>
2017-03-01 17:32:59 +0800

    Update BaseHeader.hpp

--- code_memo/BaseHeader.hpp
+++ code_memo/BaseHeader.hpp
@@ -39,4 +39,50 @@ union CharAndShort {
 	
 }
 
+//=====
+//============
+//============================
+#include <sstream> 
+#include <stdio.h>
+#include <time.h>
+
+namespace log2file {
+
+	static const char * g_path = "../../login_mode.txt";
+	static char g_buf[32] = { 0 };
+	static inline const char * GetTimeStr()
+	{
+		time_t n_time = time(NULL);
+		tm* tm_time;
+		tm_time = localtime(&n_time);
+
+		memset(g_buf, 0, sizeof(g_buf));
+#if defined(WIN32) || defined(WIN64)
+		sprintf_s(g_buf, sizeof(g_buf), "[%4d-%02d-%02d %02d:%02d:%02d] ", tm_time->tm_year + 1900,
+			tm_time->tm_mon + 1, tm_time->tm_mday, tm_time->tm_hour, tm_time->tm_min, tm_time->tm_sec);
+#else
+		snprintf(g_buf, sizeof(g_buf), "[%4d-%02d-%02d %02d:%02d:%02d] ", tm_time->tm_year + 1900,
+			tm_time->tm_mon + 1, tm_time->tm_mday, tm_time->tm_hour, tm_time->tm_min, tm_time->tm_sec);
+#endif
+		return g_buf;
+	}
+
+	static inline void Add2file(const char * str_fun ,std::string str_content)
+	{
+		FILE  * handle = fopen(g_path , "a");
+		if (handle) {
+			std::string str_ = GetTimeStr();
+			str_ += str_fun;
+			str_ += "\t";
+			str_ += str_content + "\n";
+			fputs(str_.c_str(), handle);
+			fclose(handle);
+		}
+	}
+	//__FUNCTION__
+#define _Add2file(x) Add2file(__FUNCTION__ , (x) )
+
+
+}
+
 
