e669be6ea83d8e961ef0270145c51bc1198a9313
hbb <huang_binbin1@163.com>
2017-05-11 19:59:04 +0800

    f

--- 脚本/阴阳师/find_job.py
+++ 脚本/阴阳师/find_job.py
@@ -37,6 +37,7 @@ class Job:
 	
 def AddJob():
 	#Job("" , "")
+	Job("座敷童子" , "http://www.paopaoche.net/sj/109131.html")
 	Job("食发鬼" , "http://www.paopaoche.net/sj/109095.html")
 	Job("兵俑" , "http://www.paopaoche.net/sj/109118.html")
 	Job("花_舞_桃花妖" , "http://www.paopaoche.net/sj/109022.html")
