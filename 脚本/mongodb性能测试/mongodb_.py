03df9999e444c30a018fbd94d47a24a585f1b4df
hbb <huang_binbin1@163.com>
2017-06-23 16:05:27 +0800

    add

---
+++ 脚本/mongodb性能测试/mongodb_.py
@@ -0,0 +1,55 @@
+﻿#!/usr/bin/env python 
+#encoding: utf-8
+
+import os
+import sys
+import time
+import run_text
+import pymongo
+
+
+#设置工作目录
+g_old_path = ""
+def SetPath():
+	global g_old_path
+	g_old_path = os.getcwd()
+	os.chdir(os.path.split(os.path.realpath(__file__))[0] )
+	
+def ResetPath():
+	os.chdir(g_old_path )
+
+
+connection = pymongo.MongoClient('192.168.0.49',27017)
+
+hbb_db = connection['hbb_db']
+role_table = hbb_db["role"]
+
+g_total = 10000/ run_text.g_run_times
+
+if 0:
+	raw_input("[begin] input one to clean\n")
+	for one_ in role_table.find():
+		role_table.remove(one_)
+	print "clean ok"
+	os._exit(0)
+else:
+	pass
+	#raw_input("[begin] input one to start\n")
+
+n_begin_t = time.time()
+for i in range(g_total):
+	o_data = {'name':'zhou', 'lv': '22', 'exp': '880'}
+	role_table.insert(o_data)
+print "g_total:",g_total
+str_dirr = str(time.time() - n_begin_t)
+list_diff = str_dirr.split(".")
+run_text.Add2File(str_dirr)
+print "n_diff_t:", list_diff[0] , "s" , list_diff[1][0:3] , "ms"
+
+if 0:
+	for one_ in role_table.find():
+		print one_
+
+print role_table.count()
+#os.system("pause")
+
---
+++ 脚本/mongodb性能测试/run_test.py
@@ -0,0 +1,59 @@
+﻿#!/usr/bin/env python 
+#encoding: utf-8
+
+import os
+import sys
+import time
+import pymongo
+
+
+#设置工作目录
+g_old_path = ""
+g_run_times = 50
+g_str_file = "temp1.txt"
+def SetPath():
+	global g_old_path
+	g_old_path = os.getcwd()
+	os.chdir(os.path.split(os.path.realpath(__file__))[0] )
+	
+def ResetPath():
+	os.chdir(g_old_path )
+	
+def ClearFile():
+	f = open(g_str_file , "w")
+	f.close()
+	
+def Add2File(str_):
+	f = open(g_str_file , "a")
+	f.write(str_ + "\n")
+	f.close()
+	
+def DealFile():
+	f = open(g_str_file , "r")
+	f_total = 0.0
+	for one_ in f.readlines():
+		print one_
+		f_total += float(one_)
+	f.close()
+	print "g_run_times:" , g_run_times
+	print f_total/ g_run_times , "s"
+
+if __name__ == "__main__":
+	ClearFile()
+	for i in range(g_run_times):
+		os.system("start mongodb_.py")
+
+	os.system("pause")
+	DealFile()
+	
+	connection = pymongo.MongoClient('192.168.0.49',27017)
+	hbb_db = connection['hbb_db']
+	role_table = hbb_db["role"]
+	
+	raw_input("[begin] input one to clean\n")
+	for one_ in role_table.find():
+		role_table.remove(one_)
+	print "clean ok"
+	
+	
+	
