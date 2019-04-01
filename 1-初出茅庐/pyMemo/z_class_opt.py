c7e8eeb10c9d837156353274e70ab9c7f5dc0f10
hbb <huang_binbin1@163.com>
2019-04-01 18:02:46 +0800

    wu

---
+++ 1-初出茅庐/pyMemo/z_class_opt.py
@@ -0,0 +1,43 @@
+﻿
+#类操作大全;class;
+
+
+
+
+
+#定义私有类
+class ServerCount2:
+	def __init__(self):
+		self.battlegateserver = "1"
+		self.battleserver = "1"
+		self.centerserver = "1"
+		self.dbserver = "1"
+		self.gameserver = "1"
+
+#定义静态类
+class ServerCount:
+	battlegateserver = "1"
+	battleserver = "1"
+	centerserver = "1"
+	dbserver = "1"
+	gameserver = "1"
+	gateserver = "1"
+	loginserver = "1"
+	proxyserver = "1"
+
+gServerCount = ServerCount()
+
+#获取变量#遍历类对象#反射
+def GetServerCount(strName):
+	global gServerCount
+	if strName in dir(gServerCount):
+		nCount = int(getattr(gServerCount ,strName ))
+		#setattr(gServerCount ,strName , v_)		#二次修改变量
+		print nCount
+
+
+
+
+
+
+
---
+++ 1-初出茅庐/pyMemo/z_file_opt.py
@@ -0,0 +1,44 @@
+﻿
+
+
+#文件拷贝;文件是否存在;目录是否存在
+def copy_to_dir(str_dir_0 , str_dir_1):
+	if os.path.exists(str_dir_0) == False:
+		print "##err"
+		return
+
+	if os.path.exists(str_dir_1) == False:
+		os.mkdir(str_dir_1)
+
+	for parent, dirnames, filenames in os.walk(str_dir_0):
+		for filename in filenames:
+			str_file_0 = parent +"/"+ filename
+			shutil.copy (str_file_0, str_dir_1)
+			print "#copy " , str_file_0 , "\t->>" , str_dir_1
+		break #仅仅 遍历 第一层目录
+
+#写文件
+def newfile(csv_file ):
+	if os.path.exists(g_csv_dir) == False:
+		os.mkdir(g_csv_dir)
+	f = open(g_csv_dir + csv_file , "w")
+	f.close()
+
+#追加方式打开文件
+def add2file(csv_file , str_):
+	if os.path.exists(g_csv_dir) == False:
+		os.mkdir(g_csv_dir)
+	f = open(g_csv_dir + csv_file , "a")
+	f.write(str_)
+	f.close()
+
+#创建utf类型的文件
+def InitFile(path , file_ ,str_):
+	import codecs
+	if os.path.exists(path) == False:
+		os.mkdir(path)
+	f = codecs.open(path+file_, "w", "utf-8")
+	f.write(str_)
+	f.close()
+
+
