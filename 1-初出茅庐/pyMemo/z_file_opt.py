956e596816d8554a909a53a1866aba25e70524a5
黄斌斌 <huang_binbin1@163.com>
2019-08-21 10:27:32 +0800

    hbb
    fix

--- 1-初出茅庐/pyMemo/z_file_opt.py
+++ 1-初出茅庐/pyMemo/z_file_opt.py
@@ -1,22 +1,6 @@
 ﻿
 
 
-#文件拷贝;文件是否存在;目录是否存在
-def copy_to_dir(str_dir_0 , str_dir_1):
-	if os.path.exists(str_dir_0) == False:
-		print "##err"
-		return
-
-	if os.path.exists(str_dir_1) == False:
-		os.mkdir(str_dir_1)
-
-	for parent, dirnames, filenames in os.walk(str_dir_0):
-		for filename in filenames:
-			str_file_0 = parent +"/"+ filename
-			shutil.copy (str_file_0, str_dir_1)
-			print "#copy " , str_file_0 , "\t->>" , str_dir_1
-		break #仅仅 遍历 第一层目录
-
 #写文件
 def newfile(csv_file ):
 	if os.path.exists(g_csv_dir) == False:
@@ -41,6 +25,7 @@ def InitFile(path , file_ ,str_):
 	f.write(str_)
 	f.close()
 
+
 #清空某个文件夹
 def ClearFolder(path_):
 	ls = os.listdir(path_)
@@ -51,9 +36,65 @@ def ClearFolder(path_):
 			ClearFolder(one_path)
 		else:
 			os.remove(one_path)
-
+#
 #删除文件夹
 def DelFolder(path_):
 	ClearFolder(path_)
 	os.rmdir(path_)
+#
+
+def GetFileModifyTime(filePath):
+	filePath = unicode(filePath,'utf8')
+	t = os.path.getmtime(filePath)
+	return t
+	
+#删除过期的文件
+def DelOldFile(str_dir):
+	list_ = []
+	for cur_path, dirnames, filenames in os.walk(str_dir):
+		cur_time = time.time()
+		for one in filenames:
+			fix_time = GetFileModifyTime(cur_path + one)
+			if cur_time - fix_time > 60*60*24*3:
+				list_.append(cur_path + one)
+	
+	for one_ in list_:
+		os.remove(one_)
+		print one_
+		
+	return len(list_)
+	
+#删除过期的文件
+#参数:路径列表
+def DelOldFileExt(listPath):
+	listDelFileNum = []
+	for one_ in listPath:
+		n1 = DelOldFile(one_)
+		listDelFileNum.append(n1)
+	pass
+	
+	nIndex = 0
+	for one_ in listDelFileNum:
+		print "##",listPath[nIndex] , "del file " , one_
+		nIndex +=1	#索引自增
+	#
+
+#拷贝文件
+def copy_to_dir(str_dir_0 , str_dir_1):
+	if os.path.exists(str_dir_0) == False:
+		print "##err"
+		return
+
+	if os.path.exists(str_dir_1) == False:
+		os.mkdir(str_dir_1)
+		
+	for parent, dirnames, filenames in os.walk(str_dir_0):
+		for filename in filenames:
+			str_file_0 = parent +"/"+ filename
+			shutil.copy (str_file_0, str_dir_1)
+			print "#copy " , str_file_0 , "\t->>" , str_dir_1
+		break #仅仅 遍历 第一层目录
+	
+
+
 
